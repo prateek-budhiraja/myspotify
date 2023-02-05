80ee263e1c69b9d5edd1d925d50f2f2dad5f128b
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-02-05 23:36:33 +0530

    tailwind color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				green: "#1DB954",
+				sblack: "#191414",
+			},
+		},
 	},
 	plugins: [],
 };
