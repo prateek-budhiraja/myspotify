13a49ea863b4bbf7e5c0bcc54e03c739d9882435
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-02-06 12:43:32 +0530

    setup context

--- src/App.js
+++ src/App.js
@@ -2,19 +2,38 @@ import { useEffect, useState } from "react";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import { getUserToken } from "./services/spotify.js";
+import SpotifyWebApi from "spotify-web-api-js";
+import { useStateValue } from "./context/AppContext";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-	const [user, setUser] = useState(null);
+	const [{ user, token }, dispatch] = useStateValue();
 
 	useEffect(() => {
-		const result = getUserToken();
-		if (result.access_token) {
-			setUser(result);
-		}
+		const hash = getUserToken();
 		window.location.hash = "";
+		const _token = hash.access_token;
+		if (_token) {
+			dispatch({
+				type: "SET_TOKEN",
+				token: _token,
+			});
+
+			spotify.setAccessToken(token);
+			spotify.getMe().then((user) => {
+				dispatch({
+					type: "SET_USER",
+					user,
+				});
+			});
+		}
 	}, []);
 
-	return <>{user ? <Dashboard /> : <Login />}</>;
+	console.log("👤", user);
+	console.log("👽", token);
+
+	return <>{token ? <Dashboard /> : <Login />}</>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { AppContextProvider } from "./context/AppContext";
+import reducer, { initialState } from "./context/reducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<AppContextProvider initialState={initialState} reducer={reducer}>
+			<App />
+		</AppContextProvider>
 	</React.StrictMode>
 );
