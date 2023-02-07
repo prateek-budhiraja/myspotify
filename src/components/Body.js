0a36c67d624117298c770aeecb942d6959885fed
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-02-07 23:59:01 +0530

    added header and body init

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 function Body() {
 	return (
-		<div className="col-span-8 bg-[#5b5773] bg-gradient-to-b from-transparent to-sblack">
-			I am body
+		<div className="p-4 col-span-8 bg-[#5b5773] bg-gradient-to-b from-transparent to-sblack">
+			<Header />
 		</div>
 	);
 }
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Avatar } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { useStateValue } from "../context/AppContext";
+
+function Header() {
+	const [{ user }] = useStateValue();
+	return (
+		<div className="text-white flex justify-between items-center gap-5">
+			<div className="bg-white w-[50%] min-w-[100px] text-black py-1 px-4 rounded-full flex">
+				<SearchIcon className="mr-1" />
+				<input
+					className="w-[100%] focus:outline-none"
+					type="text"
+					placeholder="Search song, artist or podcast"
+				/>
+			</div>
+			<div className="flex items-center gap-4 min-w-[200px]">
+				<Avatar src={user?.images[0].url} alt="profile" />
+				<h4 className="font-semibold">{user?.display_name}</h4>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
