79a4a4c46ff2fff125dde3bd4724414d92f83069
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-02-07 22:25:52 +0530

    footer template

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,43 @@
 import React from "react";
+import PlayCircleOutlineOutlinedIcon from "@mui/icons-material/PlayCircleOutlineOutlined";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import LoopIcon from "@mui/icons-material/Loop";
+import { Slider, Stack } from "@mui/material";
+import { VolumeUp } from "@mui/icons-material";
+import DevicesOtherIcon from "@mui/icons-material/DevicesOther";
 
 function Footer() {
 	return (
-		<div className="fixed bottom-0 left-0 h-[65px] w-[100dvw] bg-sblack text-white">
-			I am footer
+		<div className="flex items-center fixed bottom-0 left-0 h-[75px] w-[100dvw] bg-sblack text-white justify-between">
+			<div className="flex-auto flex w-[30%] px-4 items-center">
+				<img
+					className="w-[50px] h-[50px] object-cover mr-3"
+					src="https://jahnavimusic.com/jm/wp-content/uploads/2021/01/govinda-album-cover-min.jpg"
+					alt=""
+				/>
+				<div>
+					<h4 className="leading-3">Song name</h4>
+					<span className="text-xs">Singer</span>
+				</div>
+			</div>
+			<div className="flex-auto w-[40%]">
+				<div className="max-w-[350px] mx-auto flex justify-between items-center">
+					<ShuffleIcon />
+					<SkipPreviousIcon />
+					<PlayCircleOutlineOutlinedIcon className="scale-150" />
+					<SkipNextIcon />
+					<LoopIcon />
+				</div>
+			</div>
+			<div className="flex-none w-[30%] px-5">
+				<Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+					<DevicesOtherIcon />
+					<VolumeUp />
+					<Slider aria-label="Volume" size="small" className="w-10" />
+				</Stack>
+			</div>
 		</div>
 	);
 }
